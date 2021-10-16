86f6169abcd5abb17c5a32fe4d1a5d397104ba82
cbcrisel <cristhian7096@gmail.com>
2021-10-15 21:47:18 -0400

    change the login folder location and its imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/home/login/login.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
