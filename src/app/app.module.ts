0b60ed34461bf7b00c9f1d4bb6ff68b291e1f637
user <user@JTORREZA-NH01.jalasoft.local>
2021-11-07 14:47:35 -0400

    File sent to backend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -14,7 +15,8 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { HttpEventType, HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  public progress!: number;
+  public message!: string;
+  public fileUrl!: string;
+  @Output() public onUploadFinished = new EventEmitter();
+
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+  }
+
+  public uploadFile = (files: any) => {
+    if (files!.length === 0) {
+      return;
+    }
+    let fileToUpload = <File>files![0];
+    const formData = new FormData();
+    formData.append('file', fileToUpload, fileToUpload.name);
+    this.http.post(
+      environment.backendUrl + '/comics/0/files',
+      formData,
+      { reportProgress: true, observe: 'events' }
+    )
+      .subscribe((event: any) => {
+        if (event.type === HttpEventType.UploadProgress)
+          this.progress = Math.round(100 * event.loaded / event.total!);
+        else if (event.type === HttpEventType.Response) {
+          this.message = 'Upload success.';
+          this.onUploadFinished.emit(event.body);
+          console.log(event.body);
+          this.fileUrl = event.body.filePath;
+        }
+      });
   }
 
 }
