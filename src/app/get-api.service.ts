7b999aa269ff29a3877dd242f8ca1f401e47596f
Jorge Torrez Aramayo <jorgerodrigotorrez@gmail.com>
2022-04-25 19:48:39 -0400

    file uploaded successfully

--- src/app/get-api.service.ts
+++ src/app/get-api.service.ts
@@ -7,23 +7,20 @@ import { Observable } from 'rxjs';
 export class GetApiService {
 
   baseApiUrl = "https://criselito-comic-backend.herokuapp.com/api/upload";
+  // baseApiUrl = "http://localhost:8000/api/upload";
 
   constructor(private http: HttpClient) { }
 
   upload(file: any, name: string, description: string): Observable<any> {
-
     const formData = new FormData();
-
     formData.append('name', name);
     formData.append('description', description);
     formData.append("document", file, file.name);
-
     return this.http.post(this.baseApiUrl, formData)
   }
+
   iaupload(url: string): Observable<any> {
-
     const formData = new FormData();
-
     formData.append("language", 'eng');
     formData.append("isOverlayRequired", 'false');
     formData.append("url", url);
@@ -32,14 +29,9 @@ export class GetApiService {
     formData.append("filetype", 'pdf');
     // formData.append("scale", 'true');
     // formData.append("detectOrientation", 'false');
-
     let httpHeaders = new HttpHeaders();
     httpHeaders = httpHeaders.append('apikey', 'deef01629988957');
     // httpHeaders.append('apikey', 'helloworld');
-
-    console.log(httpHeaders);
-
-
     return this.http.post('https://api.ocr.space/parse/image', formData, { headers: httpHeaders });
   }
 }
